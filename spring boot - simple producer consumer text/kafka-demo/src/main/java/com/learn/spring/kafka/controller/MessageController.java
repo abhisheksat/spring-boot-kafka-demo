07cb4748532c817da158813b9c9e3fe01b0bbb58
abhisheksat <abhisheksatpute11@gmail.com>
2024-09-28 00:54:31 -0400

    Initial Commit | Spring Boot - Simple Producer Consumer Text

---
+++ spring boot - simple producer consumer text/kafka-demo/src/main/java/com/learn/spring/kafka/controller/MessageController.java
@@ -0,0 +1,25 @@
+package com.learn.spring.kafka.controller;
+
+import com.learn.spring.kafka.producer.KafkaProducer;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/messages")
+@RequiredArgsConstructor
+public class MessageController {
+
+    private final KafkaProducer kafkaProducer;
+
+    @PostMapping
+    public ResponseEntity<String> sendMessage(@RequestBody String message) {
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("Message queued successfully!");
+    }
+}
